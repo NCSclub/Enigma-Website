070ad8c66561d47058c420d0c8e9fea6113449e0
akramoo <samirounis2@gmail.com>
2024-04-03 01:15:02 +0100

    fixing the font and the sizing of the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,28 +8,25 @@ import EnigmaLogo from "../../../public/images/Footer/EnigmaLogo.svg";
 
 const Footer = () => {
   return (
-    <footer className="w-screen h-[20vh] bg-black">
-      <div className="flex p-5 justify-between items-center">
+    <footer className="w-screen h-[22vh] bg-black">
+      <div className="w-full h-full flex p-3 justify-between gap-7 items-center">
         <div>
           <Image
-            className="object-cover"
             src={EnigmaLogo}
             alt=""
             width={150}
             height={150}
           />
         </div>
-        <div className="flex flex-col gap-5 justify-center">
-          <div className="flex gap-7 justify-center">
+        <div className="flex flex-col gap-5 justify-end">
+          <div className="flex gap-5 justify-center">
             <Image
-              className="object-cover"
               src={NcsLogo}
               width={50}
               height={50}
               alt=""
             />
             <Image
-              className="object-cover"
               src={MacroLogo}
               width={50}
               height={50}
@@ -37,13 +34,13 @@ const Footer = () => {
             />
           </div>
           <div>
-            <p className="font-subtext text-white max-lg:text-sm">Made with ❤️ by NCS</p>
+            <p className="font-subtext text-white text-center max-lg:text-sm max-sm:text-xs">Made with ❤️ by NCS</p>
           </div>
         </div>
         <div>
           <button className="hover:bg-slate-400 hover:rounded-md p-2">
             <Link href="/" className="flex gap-2">
-              <span className="text-white font-text font-bold max-lg:text-sm">
+              <span className="text-white font-text max-lg:text-sm max-sm:text-xs">
                 Back to the top
               </span>
               <Image alt="" src={TopArrow} />
