1b381c731c458aecd145e6bad75e61c887fe1b81
akramoo <samirounis2@gmail.com>
2024-04-03 01:14:04 +0100

    adding title shadow bg Img for the agenda section

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,7 @@ module.exports = {
         link: ['var(--font-poppins)']
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'title-shadow': "url('/images/Agenda/titleShadow.svg')"
       },
     },
   },
