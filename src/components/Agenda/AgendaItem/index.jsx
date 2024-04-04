74d9d317c87aeca23cdddb03147f95bf211b4d93
akramoo <samirounis2@gmail.com>
2024-04-04 20:24:38 +0100

    fixing the reponsiveness of the agenda

--- src/components/Agenda/AgendaItem/index.jsx
+++ src/components/Agenda/AgendaItem/index.jsx
@@ -1,7 +1,10 @@
 const AgendaItem = ({ time, title, className }) => (
-  <div className={`flex justify-center items-center gap-4 flex-col w-40 h-40 rounded-full border-2 border-blue-500 font-text ${className}`}>
-    <span>{time}</span>
-    <span className="text-[#0EC9E3]">{title}</span>
+  <div
+    className={`flex justify-center items-center gap-2 flex-col w-40 h-40 rounded-full p-1
+  border-2 border-blue-500 font-text ${className} max-xl:w-32 max-xl:h-32 max-lg:w-28 max-lg:h-28 max-sm:w-20 max-sm:h-20`}
+  >
+    <span className="text-center max-xl:text-sm max-lg:xs max-sm:text-[10px]">{time}</span>
+    <span className="text-[#0EC9E3] text-center max-xl:text-xs max-lg:text-[10px] max-sm:text-[8px]">{title}</span>
   </div>
 );
 
