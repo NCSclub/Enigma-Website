0753b801be12ab6fc2e2261a340a613d20f49e2f
akramoo <samirounis2@gmail.com>
2024-04-04 18:56:00 +0100

    Adding focus to the buttons

--- src/components/Agenda/index.jsx
+++ src/components/Agenda/index.jsx
@@ -3,8 +3,7 @@
 import { useState, useEffect } from "react";
 
 import AgendaItem from "./AgendaItem/index.jsx";
-import { DayOneAgenda, DayTwoAgenda } from './agendaObj.js'
-
+import { DayOneAgenda, DayTwoAgenda } from "./agendaObj.js";
 
 const Agenda = () => {
   const [index, setIndex] = useState(0);
@@ -13,8 +12,8 @@ const Agenda = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex(prevIndex => (prevIndex + 1) % agenda.length);
-      setTranslate(prevTranslate => !prevTranslate);
+      setIndex((prevIndex) => (prevIndex + 1) % agenda.length);
+      setTranslate((prevTranslate) => !prevTranslate);
     }, 3000);
 
     return () => clearInterval(interval);
@@ -23,7 +22,7 @@ const Agenda = () => {
   const handleButtonClick = (day) => {
     setAgenda(day === 1 ? DayOneAgenda : DayTwoAgenda);
     setIndex(0);
-    setTranslate(false); 
+    setTranslate(false);
   };
 
   return (
@@ -33,12 +32,31 @@ const Agenda = () => {
           <h1 className="font-text text-4xl">Our event Agenda</h1>
         </div>
         <div className="flex gap-9">
-          <button onClick={() => handleButtonClick(1)} className="bg-[#15153F] h-20 w-44 rounded-lg  font-text text-2xl font-bold hover:bg-blue-500 hover:shadow-2xl hover:shadow-blue-500 hover:-translate-y-2 hover:duration-700">DAY1</button>
-          <button onClick={() => handleButtonClick(2)} className="bg-[#15153F] h-20 w-44 rounded-lg  font-text text-2xl font-bold hover:bg-blue-500 hover:shadow-2xl hover:shadow-blue-500 hover:-translate-y-2 hover:duration-700">DAY2</button>
+          <button
+            onClick={() => handleButtonClick(1)}
+            className="bg-[#15153F] h-20 w-44 rounded-lg  font-text text-2xl font-bold 
+          hover:bg-blue-500 hover:shadow-2xl hover:shadow-blue-500 hover:-translate-y-2 hover:duration-700 
+          focus:bg-blue-500 focus:shadow-2xl focus:shadow-blue-500 focus:-translate-y-2 focus:duration-700"
+          >
+            DAY1
+          </button>
+          <button
+            onClick={() => handleButtonClick(2)}
+            className="bg-[#15153F] h-20 w-44 rounded-lg  font-text text-2xl font-bold 
+          hover:bg-blue-500 hover:shadow-2xl hover:shadow-blue-500 hover:-translate-y-2 hover:duration-700 
+          focus:bg-blue-500 focus:shadow-2xl focus:shadow-blue-500 focus:-translate-y-2 focus:duration-700"
+          >
+            DAY2
+          </button>
         </div>
         <div className="h-1/3 w-2/4 p-5 bg-[#15153F] rounded-lg flex justify-center items-center gap-10">
           {agenda[index].time.map((time, i) => (
-            <AgendaItem key={i} time={time} title={agenda[index].title[i]} className={`${translate ? '-translate-y-2 duration-700' : ''}`} />
+            <AgendaItem
+              key={i}
+              time={time}
+              title={agenda[index].title[i]}
+              className={`${translate ? "-translate-y-2 duration-700" : ""}`}
+            />
           ))}
         </div>
       </div>
@@ -47,4 +65,3 @@ const Agenda = () => {
 };
 
 export default Agenda;
-
