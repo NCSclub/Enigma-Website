fd183ea93b387ca79b815d6e84afad4c66f58d27
lakehalmaya <lak.maya22@gmail.com>
2024-04-05 01:56:59 +0100

    defined the shadow as variable to use as background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,7 @@ module.exports = {
         link: ['var(--font-poppins)']
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+       "b-title": "url('/images/AboutTheClubs/shadow.svg')"
       },
     },
   },
