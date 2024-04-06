8e4f46740286236e551fb65427c7223100ea6c60
chahine54 <134723638+chahine54@users.noreply.github.com>
2024-04-06 18:49:00 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Card from './Card.jsx'
+function App() {
+
+return(
+
+  <>
+
+  <Card></Card>
+
+<hard></hard>
+  </>
+);
+
+
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,43 @@
+function Card() {
+  return (
+    <>
+      <div className='box_about_enigma'>
+        <h1 className='about_enigma'>About Enigma</h1>
+      </div>
+      <div className="all">
+        <div className="img">
+          <img className="img1" src="https://e0.pxfuel.com/wallpapers/500/871/desktop-wallpaper-pixels-250-pixels-aesthetic-pixel-art.jpg" alt="img1"></img>
+          <img className="img2" src="https://e0.pxfuel.com/wallpapers/500/871/desktop-wallpaper-pixels-250-pixels-aesthetic-pixel-art.jpg" alt="img2"></img>
+          <br />
+          <img className="img3" src="https://e0.pxfuel.com/wallpapers/500/871/desktop-wallpaper-pixels-250-pixels-aesthetic-pixel-art.jpg" alt="img3"></img>
+        </div>
+
+        <div className="text">
+            
+          <h2>Company showcase </h2>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;Explore a variety of opportunities, chat<br></br> directly with company representatives,<br></br> and gain valuable insights into your field of <br></br>interest.
+          </p>
+        </div>
+      </div>
+      <br /><br /><br /><br /><br /><br />
+      <div className="all2">
+        <div className="text2">
+          <h2>Company showcase</h2>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            The Enigma Ideathon is your chance to <br /> tackle real-world problems faced by<br /> businesses. Companies will unveil their<br /> toughest challenges, and you'll team up to<br /> brainstorm and develop innovative <br />solutions.
+          </p>
+        </div>
+        <div className="img">
+          <img className="img33" src="https://e0.pxfuel.com/wallpapers/500/871/desktop-wallpaper-pixels-250-pixels-aesthetic-pixel-art.jpg" alt="img33"></img>
+          <br />
+          <img className="img11" src="https://e0.pxfuel.com/wallpapers/500/871/desktop-wallpaper-pixels-250-pixels-aesthetic-pixel-art.jpg" alt="img11"></img>
+          <img className="img22" src="https://e0.pxfuel.com/wallpapers/500/871/desktop-wallpaper-pixels-250-pixels-aesthetic-pixel-art.jpg" alt="img22"></img>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
