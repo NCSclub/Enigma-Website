1b07841bcdff977ac1b0f70ac78cd06edf937e22
akramoo <samirounis2@gmail.com>
2024-04-08 17:17:20 +0100

    changing the hight of the section

--- src/components/WhyWaitSection/index.jsx
+++ src/components/WhyWaitSection/index.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 const WhyWaitSection = () => {
   return (
-    <section className="w-screen h-[30vh] flex justify-center items-center gap-10 p-5">
+    <section className="w-screen h-[25vh] flex justify-center items-center gap-10 p-5">
       <div>
         <Image
           alt=""
