c57a502f820a4fbfb49e483890f6bece97f95c05
akramoo <samirounis2@gmail.com>
2024-04-08 17:33:00 +0100

    deleting unwanted config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,6 @@ module.exports = {
         link: ['var(--font-poppins)']
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
           "blueborder":"url('/imges/hero/bgtext.png')"
       },
     },
