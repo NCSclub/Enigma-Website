4d215f6084892cb52c3401c7afff3e4094af07a6
akramoo <samirounis2@gmail.com>
2024-04-08 22:26:11 +0100

    adding metadata and fixing the overflow of the page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,17 @@ import Hero from "@/components/Hero";
 import Sponsors from "@/components/Sponsors";
 import WhyWaitSection from "@/components/WhyWaitSection";
 
+export const metadata = {
+  title: "Enigma",
+  description:
+    "Enigma is a Startup Extchange hosted in NIT by NCS & Marconics Club",
+};
+
 
 const Home = () => {
   return (
-    <div className="">
-      <Header />
+    <div className="overflow-hidden relative">
+      <Header /> 
       <Hero/>
       <Sponsors/>
       <WhyWaitSection/>
