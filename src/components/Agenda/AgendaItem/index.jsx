a4dde7462ddd40665b21314bf58a32ecb5381b4f
akramoo <samirounis2@gmail.com>
2024-04-08 18:25:17 +0100

    Fixing the responsive and adding motion for button and agenda item transition

--- src/components/Agenda/AgendaItem/index.jsx
+++ src/components/Agenda/AgendaItem/index.jsx
@@ -1,11 +1,18 @@
-const AgendaItem = ({ time, title, className }) => (
-  <div
-    className={`flex justify-center items-center gap-2 flex-col w-40 h-40 rounded-full p-1
-  border-2 border-blue-500 font-text ${className} max-xl:w-32 max-xl:h-32 max-lg:w-28 max-lg:h-28 max-sm:w-20 max-sm:h-20`}
-  >
-    <span className="text-center max-xl:text-sm max-lg:xs max-sm:text-[10px]">{time}</span>
-    <span className="text-[#0EC9E3] text-center max-xl:text-xs max-lg:text-[10px] max-sm:text-[8px]">{title}</span>
-  </div>
+import { motion, AnimatePresence } from "framer-motion";
+
+const AgendaItem = ({ time, title, translate }) => (
+  <AnimatePresence>
+    <motion.div
+      initial={{ y: 0 }}
+      animate={{ y: translate ? -10 : 0 }}
+      transition={{ duration: 0.8}}
+      exit={{ y: 0 }}
+      className="flex justify-center items-center gap-2 flex-col w-40 h-40 rounded-full p-1 border-2 border-blue-500 font-text"
+    >
+      <span className="text-center">{time}</span>
+      <span className="text-[#0EC9E3] text-center">{title}</span>
+    </motion.div>
+  </AnimatePresence>
 );
 
 export default AgendaItem;
--- src/components/Agenda/index.jsx
+++ src/components/Agenda/index.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
 import AgendaItem from "./AgendaItem/index.jsx";
 import { DayOneAgenda, DayTwoAgenda } from "./agendaObj.js";
 import Button from "./AgendaButton/index.jsx";
@@ -28,22 +29,22 @@ const Agenda = () => {
   };
 
   return (
-    <section className="h-screen w-screen text-white ">
+    <section className="h-fit w-screen text-white p-7">
       <div className="h-full w-full flex flex-col justify-center items-center gap-10">
         <div className="h-24 w-96 bg-title-shadow bg-cover flex justify-center items-center">
           <h1 className="font-text text-4xl">Our event Agenda</h1>
         </div>
-        <div className="flex gap-5 mx-10">
+        <div className="flex gap-5">
           <Button onClick={() => handleButtonClick(1)}>DAY1</Button>
           <Button onClick={() => handleButtonClick(2)}>DAY2</Button>
         </div>
-        <div className="h-fit w-fit p-5 bg-[#15153F] rounded-lg flex justify-center items-center gap-3">
+        <div className="h-fit w-fit p-5 bg-[#15153F] rounded-lg flex justify-center max-md:flex-wrap items-center gap-3">
           {agenda[index].time.map((time, i) => (
             <AgendaItem
               key={i}
               time={time}
               title={agenda[index].title[i]}
-              className={`${translate ? "-translate-y-2 duration-700" : ""}`}
+              translate={translate}
             />
           ))}
         </div>
