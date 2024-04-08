b39e0fa3967fecfc975a7c0a6ba1682d580c6635
akramoo <samirounis2@gmail.com>
2024-04-08 17:33:14 +0100

    fixing conflict

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
-import Hero from "@/components/Hero";
-import Sponsors from "@/components/Sponsors";
-=======
 import Agenda from "@/components/Agenda";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import WhyWaitSection from "@/components/WhyWaitSection";
 
->>>>>>> feature/akram/header-footer
 
 const Home = () => {
   return (
     <div className="">
-<<<<<<< HEAD
-        <Hero/>
-       <Sponsors/>
-    
-=======
       <Header />
       <WhyWaitSection/>
       <Footer />
->>>>>>> feature/akram/header-footer
     </div>
   );
 }
