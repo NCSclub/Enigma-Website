837596be63764d4389999a9488d05778b0540c60
akramoo <samirounis2@gmail.com>
2024-04-09 01:50:34 +0100

    adding the bg shadow

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,25 +5,27 @@ import Sponsors from "@/components/Sponsors";
 import WhyWaitSection from "@/components/WhyWaitSection";
 import Footer from "@/components/Footer";
 
-
 export const metadata = {
   title: "Enigma",
   description:
     "Enigma is a Startup Extchange hosted in NIT by NCS & Marconics Club",
 };
 
-
 const Home = () => {
   return (
-    <div className="overflow-hidden relative">
-      <Header /> 
-      <Hero/>
-      <Agenda/>
-      <Sponsors/>
-      <WhyWaitSection/>
-      <Footer />
+    <div className="overflow-hidden relative flex flex-col gap-32">
+      <div className="flex flex-col gap-32 bg-header-shadow bg-right-top bg-no-repeat">
+        <Header />
+        <Hero />
+        <Agenda />
+      </div>
+      <div className="flex flex-col gap-32 bg-footer-shadow bg-left bg-no-repeat">
+        <Sponsors />
+        <WhyWaitSection />
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
