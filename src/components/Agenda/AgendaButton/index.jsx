951161828030ea223a237d903f896f30306f7e56
akramoo <samirounis2@gmail.com>
2024-04-09 21:04:49 +0100

    changing the w and the h of the button and the Agenda Items

--- src/components/Agenda/AgendaButton/index.jsx
+++ src/components/Agenda/AgendaButton/index.jsx
@@ -6,7 +6,7 @@ const MemoizedButton = memo(({ onClick, children }) => (
     whileHover={{ scale: 1.1 }}
     whileTap={{ scale: 0.95 }}
     onClick={onClick}
-    className="bg-[#15153F] h-20 w-44 rounded-lg font-text text-2xl font-bold max-md:h-15 max-md:w-36
+    className="bg-[#15153F] h-24 w-52 rounded-lg font-text text-2xl font-bold max-md:h-16 max-md:w-40
       hover:bg-blue-500 hover:shadow-2xl hover:shadow-blue-500 hover:duration-300 
       focus:bg-blue-500 focus:shadow-2xl focus:shadow-blue-500 focus:duration-100"
   >
