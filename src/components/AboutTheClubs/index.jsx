f6e5435eeb1c5cf3b0d61b5539d5af5060a8fc2e
lakehalmaya <lak.maya22@gmail.com>
2024-04-09 02:55:10 +0100

    changed desc content

--- src/components/AboutTheClubs/index.jsx
+++ src/components/AboutTheClubs/index.jsx
@@ -11,11 +11,11 @@ const AboutTheClubSection = () => {
           <h1 className='text-3xl font-text md:text-4xl'>Contributors</h1>
           </div>
         <div className='hidden md:grid grid-cols-2 gap-16 place-items-center'>
-          <Card logo={ncs} desc="The NCS club aims to create an inclusive platform for tech enthusiasts to improve their skills and knowledge in computer science. They offer workshops, collaborative projects, and discussions to foster growth for both beginners and experts." image={pic1} widthLogo={45} widthImage={350}/>
+          <Card logo={ncs} desc="The NCS club aims to create an inclusive platform for tech enthusiasts to improve their skills and knowledge in computer science. They offer workshops, and collaborative projects, to foster growth." image={pic1} widthLogo={45} widthImage={350}/>
           <Card logo={marconics} desc="Marconics Club, founded in April 2023, is a non-profit organization preparing students for careers through company visits and events on entrepreneurship, marketing, and supply chain management." image={pic2} widthLogo={60} widthImage={350}/>
         </div>
         <div className='grid grid-cols-1 gap-y-44 md:hidden'>
-          <Card logo={ncs} desc="The NCS club aims to create an inclusive platform for tech enthusiasts to improve their skills and knowledge in computer science. They offer workshops, collaborative projects, and discussions to foster growth for both beginners and experts." image={pic1} widthLogo={40} widthImage={270}/>
+          <Card logo={ncs} desc="The NCS club aims to create an inclusive platform for tech enthusiasts to improve their skills and knowledge in computer science. They offer workshops, and collaborative projects, to foster growth." image={pic1} widthLogo={40} widthImage={270}/>
           <Card logo={marconics} desc="Marconics Club, founded in April 2023, is a non-profit organization preparing students for careers through company visits and events on entrepreneurship, marketing, and supply chain management." image={pic2} widthLogo={60} widthImage={270}/>
         </div>
       </div>
