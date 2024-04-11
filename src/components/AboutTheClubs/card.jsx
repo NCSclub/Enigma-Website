275f38699034ddda1be9afd8eb889059b858dd5f
akramoo <samirounis2@gmail.com>
2024-04-11 10:45:47 +0100

    fixing the hight of the card

--- src/components/AboutTheClubs/card.jsx
+++ src/components/AboutTheClubs/card.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Card = ({ logo, desc, image, widthLogo, widthImage }) => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="h-96 w-80 bg-[#15151D]/60 text-[#AFAFAF] mt-9 border-t-4 rounded-t-2xl border-[#4BD2E4] rounded-b-2xl md:h-80 md:w-96">
+    <div className="relative h-[39rem] flex flex-col justify-center items-center">
+      <div className="h-2/3 w-80 bg-[#15151D]/60 text-[#AFAFAF] mt-9 border-t-4 rounded-t-2xl border-[#4BD2E4] rounded-b-2xl md:h-80 md:w-96">
         <div className="h-24 flex justify-center items-center pb-4">
           <Image
             className=""
@@ -17,7 +17,7 @@ const Card = ({ logo, desc, image, widthLogo, widthImage }) => {
           {desc}
         </p>
       </div>
-      <div className="flex justify-center pt-10 z-10 relative bottom-24">
+      <div className="relative flex justify-center pt-10 z-10 bottom-[6rem]">
         <Image
           className=""
           src={image}
