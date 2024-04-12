d61155dc36a31167d560b916d1a4f7dec42c2bb8
yasuogg <sohaibben52@gmail.com>
2024-04-12 06:01:16 +0100

    adding smoth scroller of gsap

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import AboutTheClubsSection from "@/components/AboutTheClubs";
@@ -6,22 +8,39 @@ import Sponsors from "@/components/Sponsors";
 import WhyWaitSection from "@/components/WhyWaitSection";
 import Footer from "@/components/Footer";
 import AboutTheEventSection from "@/components/AboutTheEvent";
+import gsap from 'gsap';
+import { useEffect } from "react";
+import ScrollSmoother from 'gsap-trial/ScrollSmoother';
+import ScrollTrigger from 'gsap-trial/ScrollTrigger';
 
-export const metadata = {
-  title: "Enigma",
-  description:
-    "Enigma is a Startup Extchange hosted in NIT by NCS & Marconics Club",
-};
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 
 const Home = () => {
+  useEffect(() => {
+    const smoother = ScrollSmoother.create({
+      smooth: 1,
+      effects: true,
+      smoothTouch: 0.1,
+    });
+
+    // If there's no direct destroy method, you might not need to destroy it explicitly
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+ 
+
   return (
-    <div className="overflow-hidden relative flex flex-col gap-32">
-      <div className="flex flex-col gap-32 bg-header-shadow bg-right-top bg-no-repeat">
-       <div className="  flex flex-col gap-12 ">
+    <div id="smooth-wrapper" className=" overflow-hidden relative flex flex-col gap-32">
+      <div id="smooth-content">
+      <div className=" smooth-content flex flex-col gap-32 bg-header-shadow bg-right-top bg-no-repeat">
+       <div className="   flex flex-col gap-12 ">
         <Header />
         <Hero />
         </div>
-        <AboutTheClubsSection />
+        <AboutTheClubsSection  />
         <AboutTheEventSection/>
         <Agenda />
       </div>
@@ -29,6 +48,7 @@ const Home = () => {
         <Sponsors />
         <WhyWaitSection />
         <Footer />
+      </div>
       </div>
     </div>
   );
