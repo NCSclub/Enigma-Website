840792f3d481cbe67dc09949631835bb12df35cb
akramoo <samirounis2@gmail.com>
2024-04-12 20:05:58 +0100

    updating the agenda obj to the reel

--- src/components/Agenda/agendaObj.js
+++ src/components/Agenda/agendaObj.js
@@ -1,23 +1,35 @@
 const DayOneAgenda = [
-    {
-      time: ["8:00 - 9:00", "9:00 - 10:00", "10:00 - 11:00"],
-      title: ["Registration", "Opening Ceremony", "Keynote Speaker"],
-    },
-    {
-      time: ["11:00 - 12:00", "12:00 - 13:00", "13:00 - 14:00"],
-      title: ["Panel Discussion", "Lunch Break", "Workshop Session 1"],
-    },
-  ];
-  
-  const DayTwoAgenda = [
-    {
-      time: ["9:00 - 10:00", "10:00 - 11:00", "11:00 - 12:00"],
-      title: ["Networking Breakfast", "Guest Speaker", "Interactive Session"],
-    },
-    {
-      time: ["12:00 - 13:00", "13:00 - 14:00", "14:00 - 15:00"],
-      title: ["Lunch", "Panel Discussion", "Closing Ceremony"],
-    },
-  ];
-  
+  {
+    time: ["8:30 - 8:45", "8:45 - 9:30", "9:30 - 15:00"],
+    title: ["Check in", "Opening Ceremony", "Exhibitions throughout the day"],
+  },
+  {
+    time: ["14:00 - 15:00", "15:00 - 15:15", "15:15 - 16:00"],
+    title: [
+      "Conference and Panel",
+      "Closure and Certificate Distribution",
+      "Coffee Break",
+    ],
+  },
+  {
+    time: ["9:30 - 12:00", "12:00 - 13:00"],
+    title: ["Workshops", "Lunch Break"],
+  },
+];
+
+const DayTwoAgenda = [
+  {
+    time: ["8:00 - 8:30", "8:30 - 9:00", "9:00 - 12:00"],
+    title: ["Check in", "Opening Ceremony", "Ideathon kickoff"],
+  },
+  {
+    time: ["15:00 - 17:00", "17:00 - 17:30", "17:30"],
+    title: ["Judging the Presentations", "Break", "Winners Announcement"],
+  },
+  {
+    time: ["12:00 - 13:00", "13:00 - 15:00"],
+    title: ["Lunch Break", "Back to Ideathon"],
+  },
+];
+
 export { DayOneAgenda, DayTwoAgenda };
