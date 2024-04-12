be48b63b6ec18eca4f78b144824982812653b314
akramoo <samirounis2@gmail.com>
2024-04-12 20:06:53 +0100

    changing the x padding

--- src/components/Agenda/AgendaItem/index.jsx
+++ src/components/Agenda/AgendaItem/index.jsx
@@ -7,7 +7,7 @@ const AgendaItem = ({ time, title, translate }) => (
       animate={{ y: translate ? -10 : 0 }}
       transition={{ duration: 0.8}}
       exit={{ y: 0 }}
-      className="flex justify-center items-center gap-2 flex-col w-60 h-60 rounded-full p-1 border-2 border-blue-500 font-text"
+      className="flex justify-center items-center gap-2 flex-col w-60 h-60 rounded-full px-3 border-2 border-blue-500 font-text"
     >
       <span className="text-center text-3xl">{time}</span>
       <span className="text-[#0EC9E3] text-center text-xl">{title}</span>
